c65d5a00dd844c3b77ec54cbe4d6524f2b716c89
jiale <shanke607@qq.com>
2024-12-17 10:11:33 +0100

    v1.1 fixed

--- script/plantf_benchmarks.sh
+++ script/plantf_benchmarks.sh
@@ -16,7 +16,7 @@ for challenge in $CHALLENGES; do
         worker.threads_per_node=16 \
         experiment_uid=$SPLIT/$planner \
         verbose=true \
-        planner.imitation_planner.planner_ckpt="/home/jiale/Documents/checkpoints/0412_1.1.ckpt"
+        planner.imitation_planner.planner_ckpt="/home/jiale/Documents/checkpoints/1112_1.0.ckpt"
 done
 
 
