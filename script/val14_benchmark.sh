9188cb039b2a495ae682e66ce2a8b15fb4176c80
jiale <shanke607@qq.com>
2025-01-26 12:20:01 +0100

    minor update

--- script/val14_benchmark.sh
+++ script/val14_benchmark.sh
@@ -4,19 +4,18 @@
 CKPT_ROOT="~/Documents/checkpoints/"
 
 PLANNER="planTF"
-SPLIT=$1
 CHALLENGES="closed_loop_nonreactive_agents closed_loop_reactive_agents open_loop_boxes"
 
 for challenge in $CHALLENGES; do
     python run_simulation.py \
         +simulation=$challenge \
         planner=$PLANNER \
-        scenario_builder=nuplan_challenge \
-        scenario_filter=$SPLIT \
+        scenario_builder=nuplan \
+        scenario_filter=val14 \
         worker.threads_per_node=30 \
-        experiment_uid=$SPLIT/$planner \
+        experiment_uid=val14/$planner \
         verbose=true \
-        planner.imitation_planner.planner_ckpt="/home/jiale/Documents/checkpoints/2201_1.2.ckpt"
+        planner.imitation_planner.planner_ckpt="/home/jiale/Documents/checkpoints/2211_1.4_nopre.ckpt"
 done
 
 
