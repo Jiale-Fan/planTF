20f421a2d8d47d0fd6ef6a7ca097c599ec199dc6
jiale <shanke607@qq.com>
2025-05-07 11:44:56 +0200

    val14 script update

--- script/val14_benchmark.sh
+++ script/val14_benchmark.sh
@@ -15,7 +15,7 @@ for challenge in $CHALLENGES; do
         worker.threads_per_node=30 \
         experiment_uid=val14/$planner \
         verbose=true \
-        planner.imitation_planner.planner_ckpt="/home/jiale/Documents/checkpoints/famo_ft_may.ckpt"
+        planner.imitation_planner.planner_ckpt="/home/jiale/Documents/checkpoints/ft_famo_may_mec_loss_abla.ckpt"
 done
 
 
